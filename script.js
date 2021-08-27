c81b9931a6dd8ed6c5b2784fce3afc1f60ac9fb2
Nick Fernandez <nbfern90@gmail.com>
2021-08-27 13:27:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+async function getCoderData() {
+
+  var response = await fetch("https://api.github.com/users/Nbfern90");
+ 
+  var coderData = await response.json();
+    console.log(coderData);
+    document.getElementById("info").innerHTML =coderData.followers
+  return coderData;
+
+   
+  
+  
+}
+
+
